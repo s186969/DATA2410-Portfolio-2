a650fce1d3df8f57a9ad577dc16e538da03bc0dc
KristinHauso <89998828+KristinHauso@users.noreply.github.com>
2023-04-24 14:58:17 +0200

    Satt inn kommentarer på metoder

---
+++ src/DRTP.py
@@ -0,0 +1,9 @@
+# Her må vi opprette three way handshake 
+    # Bruker flaggene som ligger i header for å sende SYN, ACK
+# Gracefully close when the transfer is finished
+    # Sender sends FIN-packet. Receiver sends an ACK når FIN er received.
+    # Etter at receiver har sendt ACK så lukkes connection.
+
+
+# Construct the packets and acknowledgements
+

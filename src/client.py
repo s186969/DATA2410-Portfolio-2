5920b719fb3812c34d96216eeb3b02a3095868ff
Sultan Avtajev <sultan_avtajev@hotmail.com>
2023-05-02 16:11:30 +0200

    Sr funksjon ferdig
    Testet med både skip_ack på server side og loss fra client side

--- src/client.py
+++ src/client.py
@@ -1,3 +1,4 @@
+import time
 from application import *
 from drtp import *
 from header import *
@@ -261,141 +262,136 @@ def send_data(client_socket, file_name):
     client_socket.send(FIN_packet)
     print('Nå har clienten sendt FIN - sending ferdig')
 
+    sys.exit()
+
+# Funksjon for å sende pakker i en Selective Repeat-protokoll
+# Argumenter: 
+# client_socket: Socket for kommunikasjon med serveren
+# file_name: Filnavn for filen som skal sendes
+# testcase: Testcase for å simulere pakketap
+# window_size: Størrelse på senderens vindu
 def sel_rep(client_socket, file_name, testcase, window_size):
-    sender_window = []
-    print(f'Window: {sender_window}')
-    # Lagrer sekvensnummer og tidspunkt for sendte pakker som venter på ACK
+
+    # Funksjon for å sende en enkelt pakke gitt sekvensnummer og bilde data
+    # Argumenter:
+    # seq_num: Sekvensnummer for pakken som skal sendes
+    # image_data: Bilde data som skal sendes
+    def send_packet(seq_num, image_data):
+        # Beregne startindeksen for bildedata i pakken
+        image_data_start = 1460 * (seq_num - 1)
+        # Beregne sluttindeksen for bildedata i pakken
+        image_data_stop = image_data_start + 1460
+        # Hente bildedata for denne pakken
+        data = image_data[image_data_start: image_data_stop]
+        # Opprette pakken med sekvensnummer og data
+        packet = create_packet(seq_num, 0, 0, 64000, data)
+        # Sende pakken til serveren
+        client_socket.send(packet)  
+        # Returnere tidspunktet pakken ble sendt
+        return time.time()  
+
+    # Initialisere variabler
+    sender_window = []  # Liste over pakker i senderens vindu
+    # Array med pakker som ikke har mottatt ACK, med sekvensnummer som indeks og sendetid som verdi
     unacked_packets = {}
-    number_of_data_sent = 0
-    seq_client = 1
-    last_ack = 0
-    retransmission_timeout = 0.5  # Timeout for å sende pakken på nytt
+    # Antall data sendt (i bytes)
+    number_of_data_sent = 0 
+    # Startsekvensnummer for klienten
+    seq_client = 1  
+    # Siste mottatte ACK
+    last_ack = 0  
+    # Retransmisjonstidsavbrudd (i sekunder)
+    retransmission_timeout = 0.01  
 
-    # Leser bildet og gjør det om til bytes
-    with open(file_name, 'rb') as f:
-        image_data = f.read()
+    # Åpne filen i binærmodus og les inn bildedata
+    with open(file_name, 'rb') as f: 
+        # Les inn bildedata
+        image_data = f.read() 
 
-    # Passer på at sender_window hele tiden er fullt
+    # Hovedløkke for å sende pakker og motta ACK-er
     while True:
-        # Sjekke om det er plass i sender window til å sende en ny pakke
-        while number_of_data_sent < len(image_data) and len(sender_window) < window_size:
-             if testcase == 'loss' and seq_client == 3:
-                 print('Følgenede pakke blir ikke sendt: ', seq_client)
-                 sender_window.append(seq_client)
-                 testcase = None
-                 seq_client += 1
-                 number_of_data_sent += 1460
+        # Løkke for å sende pakker til serveren
+        while number_of_data_sent < len(image_data) and len(sender_window) < window_size: #
+            # Hvis vi simulerer tap av pakke 4
+            if testcase == 'loss' and seq_client == 4: 
+                print('Følgende pakke blir ikke sendt: ', seq_client)
+                # "Send pakken" og lagre tidspunktet den ble sendt
+                send_time = time.time()
+                # Legg til pakken i ordboken over pakker som ikke har mottatt ACK
+                unacked_packets[seq_client] = send_time
+                # Legg til pakken i senderens vindu
+                sender_window.append(seq_client) 
+                # Sett testcase til None for å ikke miste flere pakker
+                testcase = None 
+            else:
+                # Send pakken og lagre tidspunktet den ble sendt
+                send_time = send_packet(seq_client, image_data) 
+                # Legg til pakken i ordboken over pakker som ikke har mottatt ACK
+                unacked_packets[seq_client] = send_time 
+                # Legg til pakken i senderens vindu
+                sender_window.append(seq_client) 
+                # Skriv ut pakkenummer som ble sendt
+                print(f'Sendt pakke nummer: {seq_client}')
+                # Skriv ut vinduet
+                print(f'Window: [{" , ".join(map(str, sender_window))}]')
 
-                 # Break out of the inner loop when the window is full
-                 if len(sender_window) >= window_size:
-                    break
+            # Oppdater sekvensnummer og antall data sendt
+            seq_client += 1
+            # Oppdater antall data sendt
+            number_of_data_sent += 1460
 
-             # Hente ut riktig område av data
-             image_data_start = number_of_data_sent
-             image_data_stop = image_data_start + 1460
-             data = image_data[image_data_start: image_data_stop]
-
-             # Opprette pakke og sende den
-             packet = create_packet(seq_client, 0, 0, 64000, data)
-             client_socket.send(packet)
-             print(f'Sendt pakke nummer: {seq_client}')
-             # Lagrer sekvensnummer og tidspunkt for sendt pakke
-             unacked_packets[seq_client] = time.time()
-             number_of_data_sent += 1460
-
-             # Legge sendt pakke inn i sender window
-             sender_window.append(seq_client)
-             array_as_string = " ".join(str(element) for element in sender_window)
-             print(f'Window: {array_as_string}')
-             seq_client += 1
-        
-        # Sjekk om noen av de sendte pakkene har ventet på ACK for lenge
+        # Sjekk om noen pakker trenger å sendes på nytt på grunn av tidsavbrudd
         current_time = time.time()
+        # Gå gjennom alle pakker som ikke har mottatt ACK
         for seq_num, send_time in list(unacked_packets.items()):
+            # Hvis det har gått mer enn retransmission_timeout siden pakken ble sendt
             if current_time - send_time > retransmission_timeout:
+                # Skriv ut pakkenummer som er resendt på grunn av tidsavbrudd
                 print(f'Resender pakke nummer: {seq_num}')
-                image_data_start = 1460 * (seq_num - 1)
-                image_data_stop = image_data_start + 1460
-                data = image_data[image_data_start: image_data_stop]
-                packet = create_packet(seq_num, 0, 0, 64000, data)
-                client_socket.send(packet)
-                # Oppdater tidspunkt for sendt pakke
+                # Send pakken på nytt og lagre tidspunktet den ble sendt
+                send_packet(seq_num, image_data)
+                # Oppdater tidspunktet pakken ble sendt
                 unacked_packets[seq_num] = current_time
 
-        # Motta ack fra server så lenge det er pakker i sender_window
-        if len(sender_window) > 0:
-            client_socket.settimeout(0.5)
-            try:
-                receive = client_socket.recv(1472)
-                seq, ack, flags = read_header(receive)
-                print(f'Mottatt pakke med ack: {ack}')
-
-                # Hvis ack er tilhørende pakke i sender_window:
-                if ack in sender_window:
-                    # Sjekk om ack er som foventet
-                    if ack == last_ack + 1:
-                        last_ack = ack  # Oppdatere last_ack
-                        sender_window.remove(ack)
-                        # Hvis sender window nå er blitt tomt, betyr det at vi er ferdige
-                        if len(sender_window) == 0:
-                            # Sende et FIN flagg for å avslutte
-                            FIN_packet = create_packet(0, 0, 2, 64000, b'')
-                            client_socket.send(FIN_packet)
-                            print('Nå har clienten sendt FIN - sending ferdig')
-                            return
-
-                    # Fjerner pakken fra ordboken med sendte pakker som venter på ACK
-                    unacked_packets.pop(ack, None)
-            
-            except:
-                #print('Mottok ikke forventet ack')
-                # Tømmer sender window
-                #sender_window.clear()
-
-                # Sender pakken etter den forrige vi vet kom frem
-                #seq_client = last_ack + 1
-                #number_of_data_sent = 1460 * last_ack
-                print('Mottok ikke forventet ack')
-                
-                print(f'unacked_packets: {unacked_packets}')
-                
-                # Sender pakken etter den forrige vi vet kom frem
-                seq_client = last_ack + 1
-                if seq_client in unacked_packets:
-                    resend_packet = unacked_packets[seq_client]
-                    client_socket.send(resend_packet)
-                    print(f'Sender manglende pakke på nytt: {seq_client}')
-
-                number_of_data_sent = 1460 * last_ack
-        
-        # For å motta resterede ack etter siste sending
-        while len(sender_window) > 0 and number_of_data_sent >= len(image_data):
-            client_socket.settimeout(0.5)
-            try:
-                receive = client_socket.recv(1472)
-                seq, ack, flags = read_header(receive)
-                print(f'Mottatt pakke med ack: {ack}')
-
-                if ack in sender_window:
-                    sender_window.remove(ack)
-                    array_as_string = " ".join(str(element) for element in sender_window)
-                    print(f'Nå er vinduet: {array_as_string}')
-                    continue
-
-            except:
-                print('Noe gikk galt med å motta ack')
-                sender_window.clear()
-                seq_client = last_ack + 1
-                print(f'Fra except her er seq_client {seq_client} forventer 4')
-                number_of_data_sent = 1460 * last_ack
-                continue
-        
-        if number_of_data_sent >= len(image_data) and len(sender_window) == 0:
-            FIN_packet = create_packet(0, 0, 2, 64000, b'')
-            client_socket.send(FIN_packet)
-            print('Nå har clienten sendt FIN - sending ferdig')
+        # Avslutt løkken hvis sender_window er tomt
+        if not sender_window:
             break
 
+        # Sett en tidsavbrudd for å vente på ACK fra serveren
+        client_socket.settimeout(0.5)
+        try:
+            # Motta pakke fra serveren
+            receive = client_socket.recv(1472)
+            # Les headerinformasjonen fra pakken
+            seq, ack, flags = read_header(receive)
+            # Skriv ut ACK som er mottatt
+            print(f'Mottatt pakke med ack: {ack}')
+
+            # Hvis ACK er i senderens vindu
+            if ack in sender_window:
+                # Oppdater siste mottatte ACK
+                last_ack = ack  
+                # Fjern pakken fra senderens vindu
+                sender_window.remove(ack) 
+                # Fjern pakken fra listen over ikke-ACK-pakker
+                unacked_packets.pop(ack, None)
+        except:
+            # Hvis det ikke mottas en forventet ACK
+            print('Mottok ikke forventet ack')
+            # Tøm senderens vindu og start sendingen fra siste mottatte ACK
+            sender_window.clear()
+            # Sett sekvensnummeret til å være lik siste mottatte ACK
+            seq_client = last_ack + 1
+            # Sett antall data sendt tilsvarende
+            number_of_data_sent = 1460 * last_ack
+
+    # Når alle pakker er sendt, send en FIN-pakke for å signalisere at sendingen er fullført
+    FIN_packet = create_packet(0, 0, 2, 64000, b'')
+    # Send FIN-pakken
+    client_socket.send(FIN_packet)
+    # Skriv ut at FIN-pakken er sendt
+    print('Nå har clienten sendt FIN - sending ferdig')
+    # Motta FIN-ACK fra serveren
     sys.exit()
 
 # selective-Repeat():
--- src/server.py
+++ src/server.py
@@ -149,63 +149,66 @@ def stop_and_wait(server_socket, args):
             # Sender ack-pakken til client
             server_socket.sendto(ACK_packet, address)
 
+# Definerer funksjonen 'sel_rep_server' med parametrene 'server_socket' og 'args'
 def sel_rep_server(server_socket, args):
-   buffer = {}
-   received_data = b''
-   seq_last_packet = 0
+    # Initialiserer en tom ordbok (dictionary) kalt 'buffer' for å holde pakker som kommer i feil rekkefølge.
+    buffer = {}
+    # Initialiserer en tom byte-streng kalt 'received_data' for å holde den mottatte dataen.
+    received_data = b''
+    # Initialiserer en variabel 'seq_last_packet' for å holde sekvensnummeret til den sist mottatte pakken.
+    seq_last_packet = 0
 
-   while True:
-       # Receiving a message from a client
-       tuple = server_socket.recvfrom(1472)
-       data = tuple[0]
-       address = tuple[1]
+    # Starter en evig løkke for å motta pakker fra klienten.
+    while True:
+        # Mottar data og adresse fra klienten ved hjelp av server_socket.
+        data, address = server_socket.recvfrom(1472)
+        # Henter headerinformasjonen fra den mottatte dataen.
+        header_from_data = data[:12]
+        # Parser headerinformasjonen for å hente sekvensnummeret, ACK-nummeret, flaggene og vindusstørrelsen.
+        seq, ack, flags, win = parse_header(header_from_data)
+        # Skriver ut informasjon om den mottatte pakken.
+        print(f'Pakke: {seq}, flagget: {flags}, størrelse: {len(data)}')
 
-       # Hente ut og lese av header
-       header_from_data = data[:12]
-       seq, ack, flags, win = parse_header(header_from_data)
-       print(f'Pakke: {seq}, flagget: {flags}, størrelse: {len(data)}')
-
-       if flags == 2:
-           # Sjekke om pakken som er mottatt inneholder FIN-flagg
-           print('Mottatt FIN flagg fra clienten, mottar ikke mer data')
-           # Når FIN flagg er mottatt skriver vi dataen til filen.
-           mengde = len(received_data)
-           print(f'Received data er: {mengde}')
-           with open('received_image.jpg', 'wb') as f:
+        # Sjekker om mottatt pakke har FIN-flagget satt (flags == 2).
+        if flags == 2:
+            # Skriver ut informasjon om at FIN-flagget er mottatt og ingen flere data vil bli mottatt.
+            print('Mottatt FIN flagg fra clienten, mottar ikke mer data')
+            # Skriver ut lengden på den mottatte dataen.
+            print(f'Received data er: {len(received_data)}')
+            # Skriver den mottatte dataen til en fil kalt 'received_image.jpg'.
+            with open('received_image.jpg', 'wb') as f:
                 f.write(received_data)
-           break
+            # Avslutter evig løkke og avslutter funksjonen.
+            break
 
-       # Sjekker om pakken er den neste i rekkefølge
-       if seq == seq_last_packet + 1:
-           # Sjekke om pakken vi har fått er den neste i rekkefølge
-           if args.testcase == 'skip_ack' and seq == 42:
-               print('Sender ikke ack')
-               args.testcase = None
-               continue
-           else:
-               # Oppretter en tilhørende ack-pakke ved å sette ack til å være seq
-               ACK_packet = create_packet(0, seq, 0, 64000, b'')
-               # Sender ack-pakken til client
-               server_socket.sendto(ACK_packet, address)
-               print(f'Sender ack: {seq}')
-               # Legger til pakken i de mottatte dataene
-               received_data = receive_data(data, received_data)
-               seq_last_packet = seq
-
-               # Sjekker om det er pakker i bufferen som kan legges til de mottatte dataene
-               while seq_last_packet + 1 in buffer:
-                   seq_last_packet += 1
-                   received_data = receive_data(buffer[seq_last_packet], received_data)
-                   del buffer[seq_last_packet]
-       else:
-           if (seq != 0):
-               # Lagrer pakken i bufferen
-               print('Pakken kom i feil rekkefølge, lagrer i buffer og sender ack')
-               buffer[seq] = data
-               # Sender ack-pakken til client
-               ACK_packet = create_packet(0, seq, 0, 64000, b'')
-               server_socket.sendto(ACK_packet, address)
-               continue
+        # Sjekker om sekvensnummeret til den mottatte pakken er en mer enn sekvensnummeret til den sist mottatte pakken.
+        if seq == seq_last_packet + 1:
+            # Håndterer et testcase hvor serveren skal hoppe over å sende en ACK for pakke med sekvensnummer 42.
+            if args.testcase == 'skip_ack' and seq == 42:
+                print('Sender ikke ack')
+                args.testcase = None
+            else:
+                # Oppretter en ACK-pakke for den mottatte pakken og sender den til klienten.
+                ACK_packet = create_packet(0, seq, 0, 64000, b'')
+                server_socket.sendto(ACK_packet, address)
+                print(f'Sender ack: {seq}')
+                # Legger til data fra den mottatte pakken i 'received_data'.
+                received_data = receive_data(data, received_data)
+                # Oppdaterer sekvensnummeret til den sist mottatte pakken.
+                seq_last_packet = seq
+                # Hvis det er pakker i bufferet med sekvensnummeret en mer enn den sist mottatte pakken, behandles de og fjernes fra bufferet
+                while seq_last_packet + 1 in buffer:
+                    seq_last_packet += 1
+                    received_data = receive_data(
+                        buffer[seq_last_packet], received_data)
+                    del buffer[seq_last_packet]
+                    
+        # Hvis den mottatte pakken har et annet sekvensnummer enn 0 og ikke er en etter den sist mottatte pakken, lagres den i bufferet og en ACK sendes til klienten.
+        elif seq != 0:
+            print('Pakken kom i feil rekkefølge, lagrer i buffer og sender ack')
+            buffer[seq] = data
+            ACK_packet = create_packet(0, seq, 0, 64000, b'')
+            server_socket.sendto(ACK_packet, address)
 
 # Stop and wait
     # wait for packet
